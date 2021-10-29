1ad9455b7635cc11c45550d79f84f27dd992ec37
Rahul Goyal <rahulgoyal8312@gmail.com>
2021-10-29 19:32:29 +0530

    Query params addition in routes programmatically

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link, useHistory, NavLink } from "react-router-dom"
 
 const Home = () => {
+    const history = useHistory()
+
+    const handleRedirection = () => {
+        history.push({
+            pathname: "/product",
+            search: "search=something"
+        })
+    }
+
     return (
         <>
             <h1>Default Message</h1>
             {/* <a href="/product">Product</a> */}
-            <Link to={"/product"}>Product</Link>
+            <ul>
+                <li><Link to={"/product"}>Product</Link></li>
+                <li><Link to={"/product?search=something"}>Product with Search Something</Link></li>
+                <li><Link to={{
+                    pathname: "/product",
+                    search: "search=mobile"
+                }}>Product with Search Mobile</Link></li>
+                <li>
+                    <NavLink to={{
+                        pathname: "/product",
+                        search: "search=usingNavLink"
+                    }}>Product with Query String</NavLink>
+                </li>
+                <li>
+                    <button onClick={handleRedirection}>Product Redirection using Button</button>
+                </li>
+            </ul>
         </>
     )
 }
